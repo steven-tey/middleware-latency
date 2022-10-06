43d3a462b6bfaeeaadcff3c9d9f847d645ee7252
Steven Tey <stevensteel97@gmail.com>
2022-10-05 19:00:06 -0500

    added barebones middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     */
+    "/((?!api|_next)",
+  ],
+};
+
+export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const path = req.nextUrl.pathname;
+  const key = decodeURIComponent(path.split("/")[1]);
+
+  if (key === "vercel") {
+    return NextResponse.redirect("https://vercel.com");
+  }
+
+  return NextResponse.next();
+}
